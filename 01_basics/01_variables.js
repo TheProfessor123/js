d11bfb69e0b448b2540e1de43f7a6d9d493761e8
TheProfessor123 <chaganramchoudhary7@gmail.com>
2024-07-25 20:34:14 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 2106
+let accountEmail = "chaganramchoudhary@gmail.com"
+var accountPassword = "123456"
+accountCity = "Bhubaneswar"
+let accountState;
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+let name = "chhagan"
+let age = 21
+let isLoggedIn = true
+
+console.log(typeof null);
+console.log(typeof undefined);
