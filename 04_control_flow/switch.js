d891549546cac3dd5980056fd9fe2b606184a5de
TheProfessor123 <chaganramchoudhary7@gmail.com>
2024-08-02 20:07:01 +0530

    control flow code files

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch(month){
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+    default:
+        console.log("default case match");
+        break;
+}
